a248ff24bb6927ff298a7ed466700f30531d2bba
artem <artem.a.malyshev.2008@gmail.com>
2023-01-24 14:39:48 +0300

    Added Timefilter

--- main.py
+++ main.py
@@ -14,9 +14,34 @@ if __name__ == '__main__':
     porog = findMagicLine(piks)
 
     out = []
-    # Filter
+    # PikFilter
     for pik in piks:
         if pik.value >= porog: out.append(pik)
+    #TimeFilter
+    for i in range (len(out)):
+        if out[i].time +0.3 >= out[i+1].time:
+            if out[i].value > out[i + 1].value:
+                    out.pop(i+1)
+            elif out[i].value < out[i + 1].value:
+                    out.pop(i)
+
+        elif out[i-1].time +0.3 >= out[i].time:
+                if out[i-1].value > out[i].value:
+                    out.pop(i)
+                elif out[i-1].value < out[i].value:
+                    out.pop(i-1)
+        if i + 1 == len(out) - 1:
+            break
+
+
+
+
+
+
+
+
+
+
 
 
     print("Done")
